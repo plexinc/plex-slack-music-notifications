5e26cfbf9332fa78bb723bc913a630891c244f0a
Elan Feingold <elan@plex.tv>
2018-02-21 15:59:52 -1000

    First commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+In order to run this app:
+ 
+- Install [node.js](https://nodejs.org/en/).
+- Clone the repository.
+- Install dependencies using `npm install`.
+- Figure out the identifier of the player you want to control (e.g. in Plexamp look for user:identifier in config.json)
+- Make yourself a legacy slack token on [this page](https://api.slack.com/custom-integrations/legacy-tokens).
+
+Then run the app as follows:
+
+```
+$ EMOJI=<default-emoji> TOKEN=<token> PLAYER=<player> node index.js
+```
+
+Finally, add the webhook to https://app.plex.tv/web/app#!/account/webhooks (it'll be http://localhost:10000).
---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express')
+  , request = require('request')
+  , multer  = require('multer');
+
+var upload = multer({ dest: '/tmp/' });
+var app = express();
+var isPlaying = false;
+
+app.post('/', upload.single('thumb'), function (req, res, next) {
+  var payload = JSON.parse(req.body.payload);
+
+  // If the right player is playing a track, display a notification.
+  if (payload.Player.uuid = process.env.PLAYER && payload.Account.id === 1) {
+    const common = `token=${process.env.TOKEN}`;
+    if (payload.event == "media.play" || payload.event == "media.resume") {
+      request.post({
+        headers: {'content-type' : 'application/x-www-form-urlencoded'},
+        url:     'https://slack.com/api/users.profile.set',
+        body:    `${common}&profile=%7B%22status_text%22%3A%22${payload.Metadata.grandparentTitle}%20-%20${payload.Metadata.title}%22%2C%22status_emoji%22%3A%22%3Amusical_note%3A%22%7D`
+      }, (error, response, body) => {});
+    } else if (payload.event == "media.pause" || payload.event == "media.stop") {
+      request.post({
+        headers: {'content-type' : 'application/x-www-form-urlencoded'},
+        url:     'https://slack.com/api/users.profile.set',
+        body:    `${common}&profile=%7B%22status_text%22%3A%22%22%2C%22status_emoji%22%3A%22%3A${process.env.EMOJI}%3A%22%7D`
+      }, (error, response, body) => {});
+    }
+  }
+
+  res.sendStatus(200);
+});
+
+app.listen(process.env.PORT || 10000);
