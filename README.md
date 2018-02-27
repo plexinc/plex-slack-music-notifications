36cb1986b1d766a551f0ade4a518ccd55057e6f4
Duncan Beevers <duncan@dweebd.com>
2018-02-27 12:01:38 -0800

    Log errors from Slack API and flesh out README

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 In order to run this app:
- 
+
 - Install [node.js](https://nodejs.org/en/).
 - Clone the repository.
 - Install dependencies using `npm install`.
 - Figure out the identifier of the player you want to control (e.g. in Plexamp look for user:identifier in config.json)
+```
+# grep identifier -m 1 ~/Library/Application\ Support/Plexamp/config.json
+```
 - Make yourself a legacy slack token on [this page](https://api.slack.com/custom-integrations/legacy-tokens).
 
 Then run the app as follows:
 
 ```
-$ PAUSE_EMOJI=<paused-emoji> PLAY_EMOJI=<playing-emoji> TOKEN=<token> PLAYER=<player> [USERNAME=<username>] node index.js
+$ PLAY_EMOJI="plexamp-dark" \
+  PAUSE_EMOJI="plexamp-desaturated" \
+  TOKEN=<token> \
+  PLAYER=<player>
+  [USERNAME=<username>]
+  node index.js
 ```
 
 > ⚠️ You'll need to use the USERNAME env variable if you're playing music as a shared user.
--- index.js
+++ index.js
@@ -20,17 +20,33 @@ app.post('/', upload.single('thumb'), function (req, res, next) {
         headers: {'content-type' : 'application/x-www-form-urlencoded'},
         url:     'https://slack.com/api/users.profile.set',
         body:    `${common}&profile=%7B%22status_text%22%3A%22${encodeURIComponent(payload.Metadata.originalTitle || payload.Metadata.grandparentTitle)}%20-%20${encodeURIComponent(payload.Metadata.title)}%22%2C%22status_emoji%22%3A%22%3A${process.env.PLAY_EMOJI}%3A%22%7D`
-      }, (error, response, body) => {});
+      }, (error, response, body) => {
+        const parsedBody = !error && response.statusCode === 200 && JSON.parse(body);
+        const errorMessage = error || (!parsedBody.ok && parsedBody.error);
+
+        if (errorMessage) {
+          console.log('error posting play status: ' + errorMessage);
+        }
+      });
     } else if (payload.event == "media.pause" || payload.event == "media.stop") {
       request.post({
         headers: {'content-type' : 'application/x-www-form-urlencoded'},
         url:     'https://slack.com/api/users.profile.set',
         body:    `${common}&profile=%7B%22status_text%22%3A%22%22%2C%22status_emoji%22%3A%22%3A${process.env.PAUSE_EMOJI}%3A%22%7D`
-      }, (error, response, body) => {});
+      }, (error, response, body) => {
+        const parsedBody = !error && response.statusCode === 200 && JSON.parse(body);
+        const errorMessage = error || (!parsedBody.ok && parsedBody.error);
+
+        if (errorMessage) {
+          console.log('error posting stopped / paused status: ' + errorMessage);
+        }
+      });
     }
   }
 
   res.sendStatus(200);
 });
 
-app.listen(process.env.PORT || 10000);
+const port = process.env.PORT || 10000;
+app.listen(port);
+console.log('listening on port ' + port);
